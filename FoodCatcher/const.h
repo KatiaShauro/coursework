87bfb346abd6d67b43d5d69f121d5f2518c60d17
KatiaShauro <162903137+KatiaShauro@users.noreply.github.com>
2024-05-12 21:56:17 +0300

    Delete FoodCatcher directory

--- FoodCatcher/const.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONST_H
-#define CONST_H
-static const int SIDE = 1000;
-static const int SCENE_RECT = 900;
-static const int FRUITS_SIZE = 100;
-static const int FRUITS_COUNT = 11;
-#endif // CONST_H
